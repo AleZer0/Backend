0d7b298bc79495b08a4974a40b8620254ea1c89c
Alexis Balaam Diaz Rodriguez <a7exis2003@outlook.com>
2025-01-23 15:11:32 -0600

    credenciales

--- api/config.py
+++ api/config.py
@@ -1,9 +1,9 @@
 class DevelopmentConfig:
     DEBUG = True
-    MYSQL_HOST = "localhost"
+    MYSQL_HOST = "dev.xrom.cc"
     MYSQL_USER = "root"
-    MYSQL_PASSWORD = "1q2w3e4r5t"
-    MYSQL_DB = "checador-xrom"
+    MYSQL_PASSWORD = "$psswd3My$ql"
+    MYSQL_DB = "checkermodule"
 
 
 config = {"development": DevelopmentConfig}
