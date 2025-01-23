18ee2b7f20c86374b573a1a51cf2f4eeb113526a
Alexis Balaam Diaz Rodriguez <a7exis2003@outlook.com>
2025-01-23 10:45:55 -0600

    config

---
+++ api/config.py
@@ -0,0 +1,9 @@
+class DevelopmentConfig:
+    DEBUG = True
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "1q2w3e4r5t"
+    MYSQL_DB = "checador-xrom"
+
+
+config = {"development": DevelopmentConfig}
