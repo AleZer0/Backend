1577ae6fbbdf3d12e549b79cd13640b374a46391
Alexis Balaam Diaz Rodriguez <a7exis2003@outlook.com>
2025-01-24 12:57:23 -0600

    config

--- api/app.py
+++ api/app.py
@@ -5,21 +5,18 @@ from db import db
 from routes import register_routes
 
 
-def create_app(config_name):
-    app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    CORS(app)
+app = Flask(__name__)
+CORS(app)
 
-    db.init_app(app)
-    register_routes(app)
+app.config.from_object(config["development"])
+db.init_app(app)
+register_routes(app)
 
-    @app.errorhandler(404)
-    def not_found(error):
-        return {"success": False, "message": "Page not found"}, 404
 
-    return app
+@app.errorhandler(404)
+def not_found(error):
+    return {"success": False, "message": "Page not found"}, 404
 
 
 if __name__ == "__main__":
-    app = create_app("development")
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
--- api/config.py
+++ api/config.py
@@ -2,29 +2,19 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "mysecret")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URI", "mysql+pymysql://root:1q2w3e4r5t@localhost:3306/checador-xrom"
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://root:1q2w3e4r5t@localhost:3306/checador-xrom"
     )
-
-
-user = os.getenv("MYSQL_USER")
-password = os.getenv("MYSQL_PASSWORD")
-host = os.getenv("MYSQL_HOST")
-port = os.getenv("MYSQL_PORT")
-database = os.getenv("MYSQL_DB")
 
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
-    )
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv("MYSQL_USER")}:{os.getenv("MYSQL_PASSWORD")}@{os.getenv("MYSQL_HOST")}:{os.getenv("MYSQL_PORT")}/{os.getenv("MYSQL_DB")}"
 
 
 config = {"development": DevelopmentConfig, "production": ProductionConfig}
